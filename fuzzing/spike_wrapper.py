2bc6ffca76cc99f38ecf986029c23b9c17014836
x3tb3t <x3tb3t@protonmail.com>
2018-08-31 11:09:20 +0800

    Create spike_wrapper.py

---
+++ fuzzing/spike_wrapper.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+def main():
+
+    if len(sys.argv) > 5:
+        skipfile = sys.argv[5]
+    else:
+        skipfile = ”
+
+    spikese = ‘/pentest/fuzzers/spike/src/generic_send_tcp’
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    skipvar = sys.argv[3]
+    skipstr = sys.argv[4]
+
+    os.chdir(“vulnserver”)
+    allFiles = os.listdir(“.”)
+    allFiles.sort()
+    for files in allFiles:
+        if files.endswith(“.spk”) and files != skipfile:
+            cmd = “%s %s %s %s %s %s” % (spikese, ip, port, files, skipvar, skipstr)
+        if os.system(cmd):
+            print “Stopping processing file %s” % files
+            sys.exit(0)
+
+if __name__ == “__main__”:
+    main()
+    
+    
